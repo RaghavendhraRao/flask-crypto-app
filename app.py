42d7c61a2184a1bf2bc4a1ef5fd7deb5510a8ca1
Raghavendhra-herts <rd22aak@herts.ac.uk>
2025-07-23 19:32:39 +0100

    checking the changes print statement in app

--- app.py
+++ app.py
@@ -3,12 +3,22 @@ import requests
 
 app = Flask(__name__)
 
+# @app.route('/')
+# def index():
+#     response = requests.get('https://api.coindesk.com/v1/bpi/currentprice/BTC.json')
+#     data = response.json()
+#     price = data['bpi']['USD']['rate']
+#     return render_template('index.html', price=price)
+
+
+print("This is the testing app..!")
+print("This Page will show the BTC price...!")
+
 @app.route('/')
 def index():
-    response = requests.get('https://api.coindesk.com/v1/bpi/currentprice/BTC.json')
-    data = response.json()
-    price = data['bpi']['USD']['rate']
+    # MOCK data instead of calling external API
+    price = "42,000.99"  # fake BTC price for testing
     return render_template('index.html', price=price)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
